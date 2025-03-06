a5ddf65cd4ee223affb68b7c0f279790d479a414
sudopatil <rajpravinpatil09@gmail.com>
2025-03-06 23:29:34 +0530

    update

--- odsmts-front-end/src/api.js
+++ odsmts-front-end/src/api.js
@@ -2,6 +2,10 @@ import axios from "axios";
 
 const baseURL = "http://localhost:8080/api"
 
+//const baseURL = "https://a387-2409-4081-2d07-957d-a06a-2319-e124-31db.ngrok-free.app/api"
+
+
+
 // Create Axios instance
 const apiClient = axios.create({
   baseURL,
