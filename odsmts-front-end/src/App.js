9a5b9d7347e5ba1b123062c88f73e7806980d0e6
Siddhesh Mahajan <siddheshsm24f55@gmail.com>
2025-03-07 19:09:03 +0530

    inventory

--- odsmts-front-end/src/App.js
+++ odsmts-front-end/src/App.js
@@ -1,17 +1,37 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+// import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+// import Login from "./components/pages/login.js";
+
+// function App() {
+//   return (
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path="/" element={<Login />} />
+//         <Route path="/Admin-dashboard" />
+//         <Route path="/Hospital-dashboard" />
+//         <Route path="*" element={<Navigate to="/" />} />
+//       </Routes>
+//     </BrowserRouter>
+//   );
+// }
+
+// export default App;
+
+
+import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "./components/pages/login.js";
+import AdminDashboard from "./components/pages/AdminDashboard"; // ✅ Import Admin Dashboard
+import HospitalDashboard from "./components/pages/HospitalDashboard"; // ✅ Import Hospital Dashboard
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/Admin-dashboard" />
-        <Route path="/Hospital-dashboard" />
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/Admin-dashboard" element={<AdminDashboard />} />
+      <Route path="/Hospital-dashboard" element={<HospitalDashboard />} />
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
   );
 }
 
 export default App;
+
--- odsmts-front-end/src/redux/userSlice.js
+++ odsmts-front-end/src/redux/userSlice.js
@@ -1,3 +1,41 @@
+// import { createSlice } from "@reduxjs/toolkit";
+
+// const initialState = {
+//   token: null,
+//   username: null,
+//   roleId: null,
+// };
+
+// const userSlice = createSlice({
+//   name: "user",
+//   initialState,
+//   reducers: {
+//     loginSuccess: (state, action) => {
+//       state.token = action.payload.token;
+//       state.username = action.payload.username;
+//       state.roleId = action.payload.roleId;
+//     },
+//     logout: (state) => {
+//       state.token = null;
+//       state.username = null;
+//       state.roleId = null;
+//     },
+//   },
+// });
+
+// export const { loginSuccess, logout } = userSlice.actions;
+// export default userSlice.reducer;
+
+
+
+
+// when i need to use this redux data anywere in code reffer to this
+// import { useSelector } from "react-redux";
+
+// const { username, roleId } = useSelector((state) => state.user);
+
+
+
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
@@ -25,11 +63,3 @@ const userSlice = createSlice({
 
 export const { loginSuccess, logout } = userSlice.actions;
 export default userSlice.reducer;
-
-
-
-
-// when i need to use this redux data anywere in code reffer to this
-// import { useSelector } from "react-redux";
-
-// const { username, roleId } = useSelector((state) => state.user);
